d649e66e581f207a2594a403c2a7b6bcec91ef84
Shaneal Manek <smanek@dream.(none)>
2010-03-11 23:54:12 -0500

    fixed the hash extension vulnerability in session auth, but making the signature a HMAC

--- crypto.lisp
+++ crypto.lisp
@@ -16,9 +16,8 @@
   (ironclad:byte-array-to-hex-string
    (ironclad::digest-sequence :sha256 (SB-EXT:STRING-TO-OCTETS str))))
 
-;;TODO
-;;there might be a hash extension vulnerability here
-;;would probably be safer to move to HMAC
-;;I should look into that when I have some time
 (defun get-signature (secret &rest rest)
-  (hash-password (format nil "~A-~{~A~^-~}" secret rest)))
+  (let ((hmac (ironclad:make-hmac (ironclad:ascii-string-to-byte-array secret) :sha256)))
+    (loop for data in rest
+	 do (ironclad:update-hmac hmac (ironclad:ascii-string-to-byte-array data))
+	 finally (return (ironclad:byte-array-to-hex-string (ironclad:hmac-digest hmac))))))
