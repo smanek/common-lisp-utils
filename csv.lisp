f2be7f0cbbaeb472ef9b5ff4522f45c9110efb68
Shaneal Manek <smanek@dream.(none)>
2010-05-03 07:57:02 -0400

    read-csv-steam update

--- csv.lisp
+++ csv.lisp
@@ -22,43 +22,44 @@
     (string data)
     (atom (write-to-string data))))
 
-
-
-(defun read-csv-stream (stream &key (state 0) (working-result nil) (results nil) (line nil)) 
-  "A (sort-of) finite state machine to properly parse a csv file."
-  ;;State 0 is normal unquoted state, in which we're just naively reading non-special characters
-  ;;State 1 is quoted state. We're reading all characters
-  ;;State 2 is after a second quote has been encountered. A third quote will be interpreted as a literal quote
+(defun read-csv-stream (stream &key (fn #'identity) (collect t) (state 0) (working-result nil) (results nil) (line nil))
   (let ((cur-char (read-char stream nil)))
     (cond ((null cur-char) 
            (when (or line working-result) ;;Catch in case csv doesn't have a trailing newline
-                             (setf results (cons (reverse (cons working-result line)) results))) 
+	     (setf results (cons (reverse (cons working-result line)) results)))
            (reverse results))
           ((and (= state 0) 
-                (equal cur-char #\")) (read-csv-stream stream :state 1 :results results :line line))
+                (equal cur-char #\")) (read-csv-stream stream :fn fn :collect collect :state 1 :results results :line line))
           ((and (= state 0) (equal cur-char #\,)) 
-           (read-csv-stream stream :state 0 :results results :line (cons working-result line)))
+           (read-csv-stream stream :fn fn :collect collect :state 0 :results results :line (cons working-result line)))
           ((and (= state 0) (equal cur-char #\Newline)) 
-           (read-csv-stream stream :state 0 :results (cons (reverse (cons working-result line)) results)))
+           (read-csv-stream stream :fn fn :collect collect :state 0 :results (append (if collect 
+											 (list (funcall fn (reverse (cons working-result line))))
+											 (progn (funcall fn (reverse (cons working-result line)))
+												nil)) 
+										     results)))
           ((= state 0) 
-           (read-csv-stream stream :state 0 :results results :working-result
+           (read-csv-stream stream :fn fn :collect collect :state 0 :results results :working-result
                             (concatenate 'string working-result (string cur-char)) :line line))       
           ((and (= state 1) (equal cur-char #\")) 
-           (read-csv-stream stream :state 2 :results results :line line :working-result working-result))
+           (read-csv-stream stream :fn fn :collect collect :state 2 :results results :line line :working-result working-result))
           ((= state 1) 
-           (read-csv-stream stream :state 1 :results results :line line :working-result 
+           (read-csv-stream stream :fn fn :collect collect :state 1 :results results :line line :working-result 
                             (concatenate 'string working-result (string cur-char))))        
           ((and (= state 2) (equal cur-char #\Newline)) 
-           (read-csv-stream stream :state 0 :results (cons (reverse (cons working-result line)) results)))
+           (read-csv-stream stream :fn fn :collect collect :state 0 :results (append (if collect 
+											 (list (funcall fn (reverse (cons working-result line))))
+											 (progn (funcall fn (reverse (cons working-result line)))
+												nil))
+									   results)))
           ((and (= state 2) (equal cur-char #\,)) 
-           (read-csv-stream stream :state 0 :results results :line (cons working-result line)))
+           (read-csv-stream stream :fn fn :collect collect :state 0 :results results :line (cons working-result line)))
           ((and (= state 2) (equal cur-char #\")) 
-           (read-csv-stream stream :state 1 :results results :line line :working-result 
+           (read-csv-stream stream :fn fn :collect collect :state 1 :results results :line line :working-result 
                             (concatenate 'string working-result (string cur-char))))
           ((= state 2) 
-           (read-csv-stream stream :state 2 :results results :line line))
-          ('t 
-           (format t "Slipped through the FSM. This should be impossible. Cur-char is ~A state is ~A~%" cur-char state)))))
+           (read-csv-stream stream :fn fn :collect collect :state 2 :results results :line line))
+          (t (error "Slipped through the FSM. This should be impossible. Cur-char is ~A state is ~A~%" cur-char state)))))
 
 (defun parse-comma-delimited-list (string) 
   (cond ((null string) nil)
